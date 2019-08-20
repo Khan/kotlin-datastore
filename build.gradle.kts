79a84690978846d55a22cfec1645488a2d78548a
Cora Johnson-Roberson <cora@khanacademy.org>
2019-08-20 17:42:05 -0400

    Auto-bump version to 0.1.6
    Test plan:
    - fingers crossed
    
    Auditors: lizfaubell

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,7 +21,7 @@ independent artifacts):
 """
 
 group = "org.khanacademy"
-version = "0.1.6-pre1"
+version = "0.1.6"
 
 repositories {
     jcenter()
--- google-cloud-datastore/build.gradle.kts
+++ google-cloud-datastore/build.gradle.kts
@@ -4,7 +4,7 @@ the in-code representation of entities.
 """
 
 group = "org.khanacademy"
-version = "0.1.6-pre1"
+version = "0.1.6"
 
 repositories {
     jcenter()
